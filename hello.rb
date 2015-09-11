825778877275cea0e9075b909ea00835bb83e2b7
Juliana <j-liana97@hotmail.com>
2015-09-11 09:39:46 -0300

    add an author/email comment

--- hello.rb
+++ hello.rb
@@ -1,4 +1,10 @@
+# Default is World
+# Author: Jim Weirich (jim@somewhere.com)
+# This is an unwanted but committed change
+# Default is World
+# Author: Jim Weirich
+# This is an unwanted but staged comment
+# This is a bad comment.  We want to revert it.
 # Default is "World"
 name = ARGV.first || "World"
-
 puts "Hello, #{name}!"
